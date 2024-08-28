3f5a428a05c47ec8646a6a956c6e8c8ba6919320
minjae <minjae2271@naver.com>
2024-08-28 20:35:31 +0900

    first commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,11 +5,12 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Hamed.</h1>
+        <h1 className='title no-underline'>HI, I&#39;m Min Jae.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Toronto, Canada. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+          I&#39;m a web developer seeking job internationally.
+          Here I present my interest in React & Next.js.
+          I do projects for people to see the fun side of life and
+          the same time use those opportunities for self-improvement.
         </p>
       </div>
       <div className='relative'>
